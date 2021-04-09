cebcdf96dbc6a4f2fb7307a55f4c3b4bb3561748
harsh2000yadav <harsh2000yadav@gmail.com>
2021-04-09 21:37:45 +0530

    Messages

--- src/components/MyMessage.jsx
+++ src/components/MyMessage.jsx
@@ -11,8 +11,8 @@ const MyMessage = ({message}) => {
         )
     }
     return(
-        <div className = 'message' style = {{float: 'right'}}>
-            MyMessage
+        <div className = 'message' style = {{float: 'right', marginRight: '18px', color: 'white', backgroundColor: '#3B2A50'}} > 
+            {message.text}
         </div>
     )
 }
